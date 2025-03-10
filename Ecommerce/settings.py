00ba96d3c76c28a78be1280dddb4810804a5fdee
amir-kamel <amir.kghabrial@gmail.com>
2025-03-10 04:24:37 +0200

    more fix updates for vercel moreee and moree and moreeee again

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -29,6 +29,9 @@ SECRET_KEY = config('SECRET_KEY')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
